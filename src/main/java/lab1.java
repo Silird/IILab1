1cdedff079872ed8069d2a9076166b3fb4bb1569
Silird <Silird@gmail.com>
2016-09-17 16:20:34 +0300

    Add basic work with cells

--- src/main/java/lab1.java
+++ src/main/java/lab1.java
@@ -1,5 +1,17 @@
 public class lab1 {
     public static void main(String[] args) {
-        System.out.println("Работает");
+        Cell cell = new Cell(1, 2, 3, 4, 5, 6, 7, 8, Cell.EMPTY);
+        cell.Print();
+
+        System.out.println();
+
+
+        cell = cell.Move(Cell.LEFT);
+        if (cell != null) {
+            cell.Print();
+        }
+        else {
+            System.out.println("Нет такой ячейки");
+        }
     }
 }
